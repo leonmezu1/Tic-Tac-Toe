0bf494cb1a9fd3897dba39499f4ccd10079fa0da
Leonardo Mezu <leo.mez@outlook.com>
2019-12-16 16:30:20 -0500

    Added basic user input

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-print "Hello World!\n"
+p "Welcome to Tic-Tac-Toe, Let's Game"
+p "Type 'I' for instructions and learn how to play"
+p "Type 'X' to start!"
+
+start_key = gets.chomp
+
+case start_key
+when 'I'
+	p `clear`
+	p 'This are the instructions to play'
+when 'X'
+	p `clear`
+	p "Let's play"
+else
+	"Invalid input, press any key to try again!"
+end
